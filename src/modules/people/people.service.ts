c2a5db6bca20eab4163c57782e885409017083fc
Joseph Gutierrez <t022700120@unitru.edu.pe>
2024-10-09 15:55:00 -0500

    Eliminar el DTO de “person-db” y reemplazarlo con la entity “person”

--- src/modules/people/people.service.ts
+++ src/modules/people/people.service.ts
@@ -1,23 +1,19 @@
-import { PaginationService } from '@/common/pagination/services/pagination.service';
 import {
   DOCUMENT_ALREADY_EXISTS_MESSAGE,
   PERSON_REPOSITORY,
 } from '@/core/constants';
-import { handlerExceptions } from '@/core/database/handlers';
 import { BadRequestException, Inject, Injectable } from '@nestjs/common';
-import { QueryTypes } from 'sequelize';
 import { UserDB } from '../users/dto';
-import { CreatePersonDto, PersonDB, UpdatePersonDto } from './dto';
+import { CreatePersonDto, UpdatePersonDto } from './dto';
 import { Person } from './entities';
 
 @Injectable()
 export class PeopleService {
   constructor(
     @Inject(PERSON_REPOSITORY) private readonly personRepository: typeof Person,
-    private readonly paginationService: PaginationService,
   ) {}
 
-  async create(createPersonDto: CreatePersonDto): Promise<PersonDB> {
+  async create(createPersonDto: CreatePersonDto): Promise<Person> {
     const { document } = createPersonDto;
 
     const verifyDocument = await this.verifyDocument(document);
@@ -25,37 +21,18 @@ export class PeopleService {
     if (verifyDocument)
       throw new BadRequestException(DOCUMENT_ALREADY_EXISTS_MESSAGE);
 
-    try {
-      await this.personRepository.create(createPersonDto);
-      return await this.findOneByDocument(document);
-    } catch (error) {
-      handlerExceptions(error);
-    }
+    return await this.personRepository.create(createPersonDto);
   }
 
   async update(
     user: UserDB,
     updatePersonDto: UpdatePersonDto,
-  ): Promise<PersonDB> {
+  ): Promise<Person> {
     const { document } = user;
 
     const person = await this.personRepository.findOne({ where: { document } });
 
-    await person.update(updatePersonDto);
-
-    return await this.findOneByDocument(document);
-  }
-
-  private async findOneByDocument(document: string): Promise<PersonDB> {
-    const [person] = (await this.personRepository.sequelize.query(
-      'SELECT * FROM sistemas.fn_get_person_by_document(?)',
-      {
-        type: QueryTypes.SELECT,
-        replacements: [document],
-      },
-    )) as [PersonDB];
-
-    return person;
+    return await person.update(updatePersonDto);
   }
 
   private async verifyDocument(document: string): Promise<boolean> {
