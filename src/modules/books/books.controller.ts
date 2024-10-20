9ed931782a8209f9f2f0cf9c982c72c4d4db80ca
Joseph Gutierrez <t022700120@unitru.edu.pe>
2024-10-19 22:07:38 -0500

    Refactorizar la clase ResponsePaginationDto para especificar el tipo de datos en la propiedad "rows"

--- src/modules/books/books.controller.ts
+++ src/modules/books/books.controller.ts
@@ -1,5 +1,8 @@
 import { EApiMethods, ERoutes } from '@/common/enums';
-import { CreatePaginationDto } from '@/common/pagination';
+import {
+  CreatePaginationDto,
+  ResponsePaginationDto,
+} from '@/common/pagination';
 import { ParseIdPipe } from '@/common/pipes/parse-id.pipe';
 import {
   Body,
@@ -11,10 +14,11 @@ import {
   Post,
   Query,
 } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiCreatedResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
 import { BooksService } from './books.service';
 import { CreateBookDto } from './dto/create-book.dto';
 import { UpdateBookDto } from './dto/update-book.dto';
+import { Book } from './entities';
 
 @Controller(ERoutes.BOOKS)
 @ApiTags(ERoutes.BOOKS)
@@ -22,21 +26,25 @@ export class BooksController {
   constructor(private readonly booksService: BooksService) {}
 
   @Post(EApiMethods.CREATE)
+  @ApiCreatedResponse({ type: Book })
   create(@Body() createBookDto: CreateBookDto) {
     return this.booksService.create(createBookDto);
   }
 
   @Get(EApiMethods.FIND_ALL_PAGINATE)
+  @ApiCreatedResponse({ type: ResponsePaginationDto<Book> })
   findAllPaginate(@Query() createPaginationDto: CreatePaginationDto) {
     return this.booksService.findAllPaginate(createPaginationDto);
   }
 
   @Get(EApiMethods.FIND_ONE)
+  @ApiOkResponse({ type: Book })
   findOne(@Param('id', ParseIdPipe) id: number) {
     return this.booksService.findOne(id);
   }
 
   @Patch(EApiMethods.UPDATE)
+  @ApiOkResponse({ type: Book })
   update(
     @Param('id', ParseIdPipe) id: number,
     @Body() updateBookDto: UpdateBookDto,
